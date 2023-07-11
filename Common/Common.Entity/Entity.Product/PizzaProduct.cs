37bf1ae6528187667fcd8242558b57823029b484
Dthai16gg <buithai16102003@gmail.com>
2023-07-11 12:10:51 +0700

    add entity v1.2

--- Common/Common.Entity/Entity.Product/PizzaProduct.cs
+++ Common/Common.Entity/Entity.Product/PizzaProduct.cs
@@ -1,13 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Common.Common.Entity.Entity.Product;
 
 public abstract class PizzaProduct : IEntity.IEntity
 {
     [Required]
-    [MaxLength(30, ErrorMessage = "Max length is 30 cha")]
+    [MaxLength(30, ErrorMessage = "Max length is 30 characters")]
     [MinLength(3)]
     public string? Name { get; set; }
 
     [Required] public Guid Id { get; set; }
+    //Description
+    [Required]
+    [MaxLength(1000, ErrorMessage = "Max length is 1000 characters")]
+    [MinLength(3)]
+    public string? Description { get; set; }
+    //Image
+    [Required]
+    [Column(TypeName = "image")]
+    public byte[]? Image { get; set; }
+    //Status
+    [Required]
+    public bool Status { get; set; }
+    [AllowNull]
+    public virtual Title? Title { get; set; }
 }
---
+++ Common/Common.Entity/Entity.Product/Title.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.Common.Entity.Entity.Product;
+
+public class Title : IEntity.IEntity
+{
+    [Required(ErrorMessage = "Id is required")]
+    public Guid Id { get; set; }
+    [Required(ErrorMessage = "Name is required")]
+    public string? Name { get; set; }
+}
